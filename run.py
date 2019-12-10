d166e3beaa4102844835be198d3674cb055af539
Kevin Vega <kevin.n.vega@gmail.com>
2019-12-09 22:13:31 -0800

    Added gen_graphs to run.py

--- run.py
+++ run.py
@@ -10,3 +10,5 @@ df_kmeans = generate_data(path)
 
 PlotElbow(df_kmeans, 100, 10)
 KMeansAlgorithm(df_kmeans)
+
+gen_graphs(path, list_of_neurons=[8], epochs=10) # default list_of_neurons=[8, 16, 32], epochs=100 but can take a long time to run
