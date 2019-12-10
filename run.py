d61c8a3113a9c568e0b64bf49fe21f79d2ab92e5
Husna <hsayedi@engage3.com>
2019-12-09 22:33:35 -0800

    updates

--- run.py
+++ run.py
@@ -7,10 +7,12 @@ from KMeans import generate_data, KMeansAlgorithm, PlotElbow
 path = 'data/new-york-city-airbnb-open-data/AB_NYC_2019.csv'
 
 df_kmeans = generate_data(path)
-
-print("1 - Plotting the Elbow Method with iterations=100 and K=10 clusters...")
-PlotElbow(df_kmeans, 100, 10)
+print("Starting Implementation #1: K-Means Clustering...")
+print()
+print()
+print("Step 1 - Plotting the Elbow Method with iterations=100 and K=10 clusters...")
+PlotElbow(df_kmeans, 100, 6)
 
 print()
-print("2 - Running KMeansAlgorithm now with K=4 clusters...")
+print("Step 2 - Running KMeansAlgorithm now with K=4 clusters...")
 KMeansAlgorithm(df_kmeans, 100, 4)
